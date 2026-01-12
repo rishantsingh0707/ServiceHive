07ee8026f545a80c317532693099ce4c05316268
Rishant singh <rishantsingh0707@gmail.com>
2026-01-12 20:58:37 +0530

    Added Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,11 +10,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cookieParser());
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true
 }));
+
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
