cf34eefe8c7f6acf4371ad45015e4ef1ceaa1f8c
Rishant singh <rishantsingh0707@gmail.com>
2026-01-12 17:43:00 +0530

    error inn regester route

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -38,6 +38,7 @@ export const register = async (req, res) => {
       token: generateToken(user._id)
     });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: error.message });
   }
 };
