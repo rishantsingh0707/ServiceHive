8b76231fb1bed12b43a13d9615baa696a59ddae5
Rishant singh <rishantsingh0707@gmail.com>
2026-01-12 20:54:45 +0530

    Added Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -34,7 +34,7 @@ export const register = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "strict",
         maxAge: 7 * 24 * 60 * 60 * 1000
       })
