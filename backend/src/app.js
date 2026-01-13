40418be0954b9614d2e6ab58e1085a080817dea8
Rishant singh <rishantsingh0707@gmail.com>
2026-01-13 14:29:55 +0530

    Tsting Cors with latest express-5

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,10 +12,10 @@ const app = express();
 
 app.use(cors({
   origin: process.env.FRONTEND_URL,
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
-app.options("*", cors());   // <-- THIS IS CRITICAL FOR RENDER
 
 
 app.use(cookieParser());
